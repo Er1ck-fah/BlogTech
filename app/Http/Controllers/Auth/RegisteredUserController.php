b2fb3e56f51586afa169ea8246fb4f981a3c5e85
Erick KONGNE FAH <erickkongne@gmail.com>
2022-12-25 02:43:17 +0100

    prepare project

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -36,8 +36,8 @@ class RegisteredUserController extends Controller
     {
         $request->validate([
             'name' => 'required|string|max:255',
-            'username' => 'required|string|max:55|unique:'.User::class,
-            'email' => 'required|string|email|max:255|unique:'.User::class,
+            'username' => ['required','string','max:55','unique:users'],
+            'email' => 'required|string|email|max:255|unique:users',
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
