440e37c11a3336963fafd579f1166e80fe42c607
Erick KONGNE FAH <erickkongne@gmail.com>
2022-12-26 20:02:32 +0100

    Pagination

--- resources/js/Pages/Communities/Index.vue
+++ resources/js/Pages/Communities/Index.vue
@@ -37,7 +37,7 @@
                                 </tr>
                               </thead>
                               <tbody class="divide-y divide-gray-200 bg-white">
-                                <tr v-for="community in communities" :key="community.id">
+                                <tr v-for="community in communities.data" :key="community.id">
                                   <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">{{ community.name }}</td>
                                   <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ community.slug }}</td>
                                   <td class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
@@ -54,12 +54,16 @@
                                 </tr>
                               </tbody>
                             </table>
+                            <div class="m-2 p-2">
+                                <Pagination
+                                :links="communities.links"
+                                ></Pagination>
+                            </div>
                           </div>
                         </div>
                       </div>
                     </div>
                   </div>
-
             </div>
         </div>
     </AuthenticatedLayout>
@@ -68,7 +72,7 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head, Link } from '@inertiajs/inertia-vue3';
-
+import Pagination from '../../Components/Pagination.vue'
 defineProps({
     communities:Object
 });
